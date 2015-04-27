1a50baa53abad4546e8736b0e8d5c1550e5e8af4
Sarah Kim <kimsarahe@gmail.com>
2015-04-27 10:28:16 -0700

    Final

---
+++ assets/js/script3.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+
+	var clicked = false;
+
+	$("#1").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon1").show();
+		} else {
+			clicked = false;
+			$("#icon1").hide();
+		}
+	});
+
+	$("#2").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon2").show();
+		} else {
+			clicked = false;
+			$("#icon2").hide();
+		}
+	});
+
+	$("#3").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon3").show();
+		} else {
+			clicked = false;
+			$("#icon3").hide();
+		}
+	});
+
+	$("#4").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon4").show();
+		} else {
+			clicked = false;
+			$("#icon4").hide();
+		}
+	});
+
+	$("#5").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon5").show();
+		} else {
+			clicked = false;
+			$("#icon5").hide();
+		}
+	});
+
+	$("#6").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon6").show();
+		} else {
+			clicked = false;
+			$("#icon6").hide();
+		}
+	});
+
+	$("#7").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon7").show();
+		} else {
+			clicked = false;
+			$("#icon7").hide();
+		}
+	});
+
+	$("#8").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon8").show();
+		} else {
+			clicked = false;
+			$("#icon8").hide();
+		}
+	});
+
+	$("#9").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon9").show();
+		} else {
+			clicked = false;
+			$("#icon9").hide();
+		}
+	});
+
+	$("#10").click(function() {
+		if (clicked == false) {
+			clicked = true;
+			$("#icon10").show();
+		} else {
+			clicked = false;
+			$("#icon10").hide();
+		}
+	});
+	
+
+});
